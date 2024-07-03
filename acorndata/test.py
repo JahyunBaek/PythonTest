9eefe7c48db5597b0d3830f21b566935d066cb44
BJH <baekjahyun@nongshim.co.kr>
2024-07-03 15:43:23 +0900

    first commit

---
+++ acorndata/test.py
@@ -0,0 +1,50 @@
+print("Hello Worlid")
+
+print('C:\some\name')  # here \n means newline!
+print(r'C:\some\name')  # note the r before the quote
+
+print((50 - 5*6) // 4)
+
+print(5 ** 2)  # 5 squared
+
+print(2 ** 7 ) # 2 to the power of 7
+
+print(4 * 3.2 - 1)
+
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+
+print(3 * 'un' + 'ium')
+
+print('Py' 'thon')
+
+
+py = 'Py' 
+th ='thon'
+
+print(py + th)
+
+
+text = ('Put several strings within parentheses '
+        'to have them joined together.')
+
+print(text)
+
+print(text[0])
+print(text[-1])
+print(text[-2])
+print(text[0:5])
+print(text[0:5] + text[7:9])
+
+print(text[1:])
+print(text[:2])
+print(text[:-2])
+print(text[-2:])
+
+
+python = "Python";
+print(len(python[5:6]))
+print(len(python))
